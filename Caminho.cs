d61d7dd52177731d149da8abe1c7ffb217393f5c
jppedro <52078667+jppedro@users.noreply.github.com>
2020-09-04 08:40:02 -0300

    Add files via upload

---
+++ Caminho.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labirinto
+{
+    class Caminho : IComparable<Caminho>
+    {
+        int linha, coluna;
+
+        public Caminho(int l, int c)
+        {
+            linha = l;
+            coluna = c;
+        }
+
+        public int Linha { get => linha; set => linha = value; }
+        public int Coluna { get => coluna; set => coluna = value; }
+
+        public int CompareTo(Caminho other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return "" + linha + ", " + coluna;
+        }
+    }
+}
---
+++ FrmCaminhos.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Labirinto
+{
+    public partial class FrmCaminhos : Form
+    {
+        public FrmCaminhos()
+        {
+            InitializeComponent();
+        }
+
+        string arquivo = "";
+
+        private void Btn_OFD_Click(object sender, EventArgs e)
+        {
+            if(FileDialog.ShowDialog() == DialogResult.OK)
+            {
+                arquivo = FileDialog.FileName;
+            }
+        }
+
+        private void Btn_Encontrar_Click(object sender, EventArgs e)
+        {
+            Labirinto labirinto = new Labirinto(arquivo);
+            labirinto.MostrarLabirinto(dgvLabirinto);
+            labirinto.BuscarCaminho(dgvLabirinto);
+            labirinto.Exibir(dgvCaminhos);
+        }
+    }
+}
